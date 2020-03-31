5a4e25937df2d555793596783ef181875544adc9
AboveSpring <holmberg_joel@outlook.com>
2020-03-31 11:52:17 +0200

    almost done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import { Header, Icon, Menu, Segment, Sidebar, Image } from 'semantic-ui-react'
+import { Header, Icon, Menu, Sidebar, Image, } from 'semantic-ui-react'
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import LolScreen from "./Screens/LolScreen/LolScreen";
 import HomeScreen from "./Screens/HomeScreen/HomeScreen";
 import OverWatchScreen from "./Screens/OverwatchScreen/OverWatchScreen";
 import PropTypes from 'prop-types';
-import NavBarComponent from "./Components/NavBar/NavBarComponent"
+import TeamScreen from './Screens/Teams/TeamScreen';
+import EventScreen from './Screens/Events/EventScreen';
+
 
 import './App.css';
+
 
 
 
@@ -16,77 +19,69 @@ import './App.css';
 function App() {
 
 
-  const [visible, setVisible] = useState(true)
-
-
-
-
+  const [visible, setVisible] = useState(false)
   return (
 
     <Router>
-      <div className="App">
-        <Sidebar.Pushable as={Segment}>
-          <Sidebar
-            className='sideBar'
-            as={Menu}
-            animation='slide along'
-            icon='labeled'
-            inverted
-            direction='right'
-            onHide={() => setVisible(false)}
-            vertical
-            visible={visible}
-            width='wide'
-            onClick={() => setVisible(false)}
-          >
-            <Menu.Item >
+      {visible === false ?
+        <div className="navbar">
+          <div className="app-header"><h2>GHOST GAMING</h2></div>
+          <div className='menu-icon' onClick={() => setVisible(true) & window.scrollTo(0, 0)} >
+            <h2 >MENU <Icon link name='bars' /></h2></div></div>
+        : <div className='navbar-close'></div>}
+
+      <Sidebar.Pushable>
+        <Sidebar
+          as={Menu}
+          animation='overlay'
+          icon='labeled'
+          inverted
+          direction='right'
+          onHide={() => setVisible(false)}
+          vertical
+          visible={visible}
+          width='wide'
+          onClick={() => setVisible(false)}
+        >
+          <div className='test2'>
+            <Menu.Item  >
               <Image size='tiny' centered src='https://static.coingecko.com/s/halloween/ghost_a_in_dark-19d45e5ab0a77025805542d9f9160a4d784b8fa897b836c88f22463b65761ec5.png' />
-              <Header color='grey' size='large'>Ghost Gaming</Header>
+              <h1>Ghost Gaming</h1>
             </Menu.Item>
             <Menu.Item as={Link} to='HomeScreen'>
-              <Image centered size='tiny' src='https://www.esc.watch/storage/app/uploads/public/594/d3d/5d3/594d3d5d377ea524272878.png' />
-          Counter Strike
-        </Menu.Item>
-            <Menu.Item as={Link} to='LeageOfLegends'>
-              <Image centered size='tiny' src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/03444932-1ae8-44f5-8035-dbde879dcb75/d6wic5y-bfb90e9a-43fb-44b0-a077-1fe10a7ec1a1.png' />
-          Leage of Legends
-        </Menu.Item>
-            <Menu.Item as={Link} to='OverWatch'>
-              <Image centered size='tiny' src='https://cdn.iconscout.com/icon/free/png-256/overwatch-2-569226.png' />
-          OverWatch
-        </Menu.Item>
-          </Sidebar>
+              <Header textAlign='left' inverted color='grey' ><Icon name='eject' size='tiny' />Home</Header>
 
-          <Sidebar.Pusher dimmed={visible}>
-            <Segment basic>
-              <Icon link name='hand spock' size='big' onClick={() => setVisible(true)} />
+            </Menu.Item>
+            <Menu.Item as={Link} to='Events'>
+              <Header textAlign='left' inverted color='grey'><Icon name="globe" size='tiny' />Events</Header>
+
+            </Menu.Item>
+            <Menu.Item as={Link} to='Teams'>
+              <Header textAlign='left' inverted color='grey'><Icon name="users" size='tiny' />Teams</Header>
+            </Menu.Item>
+          </div>
+        </Sidebar>
+        <Sidebar.Pusher dimmed={visible}>
+          <div className="app-wrapper">
+            <div className="app-header">
+            </div>
+            <div className="app-navbar">
+            </div>
+            <div className="app-content">
               <Switch>
                 <Route path="/HomeScreen" component={HomeScreen} />
+                <Route path="/Teams" component={TeamScreen} />
+                <Route path="/Events" component={EventScreen} />
                 <Route path="/LeageOfLegends" component={LolScreen} />
                 <Route path="/OverWatch" component={OverWatchScreen} />
                 <Route path="/" exact component={HomeScreen} />
               </Switch>
-
-            </Segment>
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-
-      </div>
-
+            </div>
+          </div>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
     </Router>
-    // <div className="app-wrapper">
-    //   <div className="app-header">
-    //     <h2>GHOST GAMING</h2>
-    //   </div>
 
-    //   <div className="app-navbar">
-
-    //   </div>
-
-    //   <div className="app-content">
-    //     <HomeScreen></HomeScreen>
-    //   </div>
-    // </div>
   );
 }
 
--- src/Screens/OverwatchScreen/OverWatchScreen.js
+++ src/Screens/OverwatchScreen/OverWatchScreen.js
@@ -1,11 +1,70 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import { Grid, Image } from 'semantic-ui-react'
+
 
 export default class OverWatchScreen extends Component {
   render() {
     return (
-      <div>
-        <h1>OverWatchScreen</h1>
-      </div>
+
+      <Fragment>
+        <p className="main-header header-margin">OverWatch</p>
+        <div className="ongoing-events-wrapper">
+          <Grid>
+            <Grid.Row columns={3}>
+              <Grid.Column>
+                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              </Grid.Column>
+              <Grid.Column>
+                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              </Grid.Column>
+              <Grid.Column>
+                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div>
+
+        <p className="main-header sub-header-margin">Matches</p>
+        <div className="upcoming-match-wrapper">
+          <Grid>
+            <Grid.Row columns={5}>
+              <Grid.Column>
+                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              </Grid.Column>
+              <Grid.Column>
+                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              </Grid.Column>
+              <Grid.Column>
+                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              </Grid.Column>
+              <Grid.Column>
+                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              </Grid.Column>
+              <Grid.Column>
+                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row columns={5}>
+              <Grid.Column>
+                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              </Grid.Column>
+              <Grid.Column>
+                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              </Grid.Column>
+              <Grid.Column>
+                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              </Grid.Column>
+              <Grid.Column>
+                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              </Grid.Column>
+              <Grid.Column>
+                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div>
+      </Fragment>
     )
   }
 }
