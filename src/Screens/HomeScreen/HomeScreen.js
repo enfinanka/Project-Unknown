3caa18fa9444722f5902d31da08f8feffdd83a00
AboveSpring <holmberg_joel@outlook.com>
2020-04-14 12:06:02 +0200

    before merge

--- src/Screens/HomeScreen/HomeScreen.js
+++ src/Screens/HomeScreen/HomeScreen.js
@@ -2,9 +2,6 @@ import React, { Fragment } from 'react'
 import { Grid, Image } from 'semantic-ui-react';
 
 import './HomeScreen.css'
-import MatchCardComponent from '../../Components/MatchCard/MatchCardComponent'
-import sweden from '../../assets/images/sweden.png'
-import CardComponent from '../../Components/CardComponent/CardComponent';
 import withHttpRequests from '../../HOCS/withHttpRequests';
 
 function HomeScreen() {
@@ -21,21 +18,15 @@ function HomeScreen() {
             <Grid>
               <Grid.Row columns={3}>
                 <Grid.Column>
-                  <CardComponent>
-                    <Image className="eventImage" src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                  </CardComponent>
+                
                 </Grid.Column>
 
                 <Grid.Column>
-                  <CardComponent>
-                    <Image className="eventImage" src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                  </CardComponent>
+                 
                 </Grid.Column>
 
                 <Grid.Column>
-                  <CardComponent>
-                    <Image className="eventImage" src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                  </CardComponent>
+                 
                 </Grid.Column>
               </Grid.Row>
             </Grid>
@@ -47,43 +38,22 @@ function HomeScreen() {
               <Grid.Row columns={5}>
 
                 <Grid.Column>
-                  <MatchCardComponent>
-                    <img src={sweden} className="swedenimg" alt="flag" />
-                    <p>VS.</p>
-                    <img src={sweden} className="swedenimg" alt="flag" />
-                    <div className="break" />
-                    <p style={{ fontSize: '1.5em' }}>NIP</p>
-                    <p style={{ fontSize: '1.5em' }}>Fnatic</p>
-                  </MatchCardComponent>
+                
                 </Grid.Column>
 
                 <Grid.Column>
-                  <MatchCardComponent>
-                    <img src={sweden} className="swedenimg" alt="flag" />
-                    <p>VS.</p>
-                    <img src={sweden} className="swedenimg" alt="flag" />
-                    <div className="break" />
-                    <p style={{ fontSize: '1.5em' }}>NIP</p>
-                    <p style={{ fontSize: '1.5em' }}>Fnatic</p>
-                  </MatchCardComponent>
+                
                 </Grid.Column>
 
                 <Grid.Column>
-                  <MatchCardComponent>
-                    <img src={sweden} className="swedenimg" alt="flag" />
-                    <p>VS.</p>
-                    <img src={sweden} className="swedenimg" alt="flag" />
-                    <div className="break" />
-                    <p style={{ fontSize: '1.5em' }}>NIP</p>
-                    <p style={{ fontSize: '1.5em' }}>Fnatic</p>
-                  </MatchCardComponent>
+                 
                 </Grid.Column>
 
                 <Grid.Column>
-                  <MatchCardComponent></MatchCardComponent>
+                
                 </Grid.Column>
                 <Grid.Column>
-                  <MatchCardComponent></MatchCardComponent>
+                 
                 </Grid.Column>
               </Grid.Row>
             </Grid>
